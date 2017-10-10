cf27ec6753c04000eabbcc07f457e8803ea81097
Florian Kempenich <Shockn745@gmail.com>
2017-10-10 21:54:07 +0100

    Introducing . . . . . The Stepmaker!!

---
+++ stepmaker.sh
@@ -0,0 +1,12 @@
+echo 'Do NOT run, but `source ./stepmaker.sh`'
+echo 'If sourced, ignore this message ;)'
+
+function step {
+  step=$1
+  comment=$2
+  cmd=${@:3}
+
+  echo "#!/bin/bash\n\n$cmd\n" > ./step-$1_$2.sh
+  sudo chmod +x ./step-$1_$2.sh
+}
+
